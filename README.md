51cce09cf2a204d9718c0cccd995aea41d2f6104
Джон Кровавое Перо <iev@nsk.jetico.com>
2023-01-22 17:43:59 +0600

    0.3.0

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ https://beta.openai.com/docs/
 
 # Usage
 
+## Completions API
+
 ```rust
 
 use openai_api_client::*;
@@ -18,12 +20,13 @@ async fn main() {
     let api_key = "............";
     let model = "text-davinci-003";
     let max_tokens:u32 = 3;
-    let prompt = "Is Biden president of USA?  If you ask yes or not. I say:";
+    let prompt = "Is Biden president of USA?  If you ask yes or not. \
+     I say:";
     let result = completions_pretty(prompt, model, max_tokens, &api_key).await;
     println!("result: {:?}", result);
     
     // hardcore usage
-    let params = Params {
+    let params = CompletionsParams {
         model: model.to_string(),
         temperature: 0,
         max_tokens: max_tokens,
@@ -40,9 +43,30 @@ async fn main() {
         logit_bias: None,
         user: None,
     };
-    let new_promt = "Is Biden president of Canada?  If you ask yes or not. I say:";
+    let new_promt = "Is Biden president of Canada?  If you ask yes or \
+     not. I say:";
     let result_hard = completions(new_promt, &params, &api_key).await;
     println!("result: {}", result_hard.unwrap().choices[0].text);
+
+}
+
+```
+
+## Edits API
+
+```rust
+
+use openai_api_client::*;
+
+
+#[actix_rt::main]
+async fn main() {
+    // pretty usage
+    let api_key = "............";
+    let result_edits:String  = edits_pretty("Helllo, Mick!", "Fix grammar", 
+                                            "text-davinci-edit-001", &api_key).await;
+    println!("result: {}", result_edits);
+
 
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use serde::Deserialize;
 use serde::Serialize;
 
 
-pub async fn completions(prompt: &str, params: &Params, api_key: &str) -> std::io::Result<Response> {
+pub async fn completions(prompt: &str, params: &CompletionsParams, api_key: &str) -> std::io::Result<CompletionsResponse> {
     let client = Client::default();
 
     let request: Request = Request {
@@ -39,7 +39,7 @@ pub async fn completions(prompt: &str, params: &Params, api_key: &str) -> std::i
     let result_bytes = resp.body().await.unwrap();
     let result_string = String::from_utf8(result_bytes.to_vec()).unwrap();
     // println!("{}", result_string);
-    let result: Result<Response, serde_json::Error> = serde_json::from_str(&result_string);
+    let result: Result<CompletionsResponse, serde_json::Error> = serde_json::from_str(&result_string);
     match result {
         Ok(response) => Ok(response),
         Err(_e) => {
@@ -51,7 +51,7 @@ pub async fn completions(prompt: &str, params: &Params, api_key: &str) -> std::i
 
 
 pub async fn completions_pretty(prompt: &str, model: &str, max_tokens: u32, api_key: &str) -> String {
-    let params = Params {
+    let params = CompletionsParams {
         model: model.to_string(),
         temperature: 0,
         max_tokens: max_tokens,
@@ -72,12 +72,7 @@ pub async fn completions_pretty(prompt: &str, model: &str, max_tokens: u32, api_
     let res = completions(prompt, &params, api_key).await;
     match res {
         Ok(response) => {
-            let mut result = String::new();
-            for choice in response.choices {
-                result.push_str(&choice.text);
-                result.push_str(" ");
-            }
-            result
+            response.choices[0].text.clone()
         }
         Err(e) => e.to_string(),
     }
@@ -98,7 +93,7 @@ pub struct ErrorResponseObject {
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct Params {
+pub struct CompletionsParams {
     pub model: String,
     pub temperature: u32,
     pub max_tokens: u32,
@@ -119,17 +114,17 @@ pub struct Params {
 
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Response {
+pub struct CompletionsResponse {
     pub id: String,
     pub object: String,
     pub created: u32,
     pub model: String,
-    pub choices: Vec<Choice>,
+    pub choices: Vec<CompletionsChoice>,
     pub usage: Usage,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
-pub struct Choice {
+pub struct CompletionsChoice {
     pub text: String,
     pub index: u32,
     pub logprobs: Option<String>,
@@ -169,6 +164,96 @@ pub struct Request {
     pub user: Option<String>,
 }
 
+#[derive(Deserialize, Serialize)]
+pub struct EditsParams {
+    pub model: String,
+    pub temperature: u32,
+    pub top_p: f32,
+    pub n: u32,
+}
+
+#[derive(Deserialize, Serialize)]
+struct RequestEdit {
+    model: String,
+    input: String,
+    instruction: String,
+    n: u32,
+    temperature: u32,
+    top_p: f32
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct EditsResponse {
+    pub object: String,
+    pub created: u32,
+    pub choices: Vec<EditsChoice>,
+    pub usage: Usage,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct EditsChoice {
+    pub text: String,
+    pub index: u32,
+}
+
+
+
+pub async fn edits(input: &str, instruction: &str, params: &EditsParams, api_key: &str) -> std::io::Result<EditsResponse> {
+    let client = Client::default();
+
+    let request: RequestEdit = RequestEdit {
+        model: params.model.clone(),
+        input: input.to_string(),
+        instruction: instruction.to_string(),
+        n: params.n,
+        temperature: params.temperature,
+        top_p: params.top_p,
+
+    };
+
+
+    let request_string = serde_json::to_string(&request).unwrap();
+    // println!("{}", request_string);
+    let mut resp = client.post("https://api.openai.com/v1/edits")
+        .insert_header(("Content-Type", "application/json"))
+        .insert_header(("Authorization", format!("Bearer {}", api_key)))
+        .send_body(request_string)
+        .await.unwrap();
+
+    let result_bytes = resp.body().await.unwrap();
+    let result_string = String::from_utf8(result_bytes.to_vec()).unwrap();
+    // println!("{}", result_string);
+    let result: Result<EditsResponse, serde_json::Error> = serde_json::from_str(&result_string);
+    match result {
+        Ok(response) => Ok(response),
+        Err(_e) => {
+            let result_err: ErrorResponse = serde_json::from_str(&result_string).unwrap();
+            Err(Error::new(std::io::ErrorKind::Other, result_err.error.message))
+        }
+    }
+}
+
+pub async fn edits_pretty(input: &str, instruction: &str, model: &str, api_key: &str) -> String {
+    let params = EditsParams {
+        model: model.to_string(),
+        temperature: 0,
+        top_p: 1.0,
+        n: 1,
+    };
+
+    let res = edits(input, instruction, &params, api_key).await;
+    match res {
+        Ok(response) => {
+            response.choices[0].text.clone()
+        }
+        Err(e) => e.to_string(),
+    }
+}
+
+
+
+
+
 #[cfg(test)]
 mod tests {
     use std::env;
@@ -180,30 +265,12 @@ mod tests {
         dotenv().ok();
         let api_key = env::var("OPEN_AI_API_KEY").expect("OPEN_AI_API_KEY must be set");
 
-        let params = Params {
-            model: "text-davinci-003".to_string(),
-            temperature: 0,
-            max_tokens: 5,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-            stop: None,
-            suffix: None,
-            n: 1,
-            stream: false,
-            logprobs: None,
-            echo: false,
-            best_of: 1,
-            logit_bias: None,
-            user: None,
-        };
+        let model = "text-davinci-003";
+        let max_tokens:u32 = 3;
+        let result:String = completions_pretty("Is Madonna president of USA? If you ask yes or not. I say:", model, max_tokens, &api_key).await;
+        println!("result: {}", result);
 
-
-        let result_hard = completions("Is Putin president of Russia? If you ask yes or not. I say:", &params, &api_key).await;
-        println!("result: {}", result_hard.unwrap().choices[0].text);
-        // let model = "text-davinci-003";
-        // let max_tokens:u32 = 3;
-        // let result = completions_pretty("Is Putin president of USA?   If you ask yes or not. I say:", model, max_tokens, &api_key).await;
-        // println!("result: {:?}", result);
+        let result_edits:String  = edits_pretty("Helsllo, Mick!", "Fix grammar", "text-davinci-edit-001", &api_key).await;
+        println!("result: {}", result_edits);
     }
 }
